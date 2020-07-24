906510ed4794e3365cd2429970fc3bfd7ab26522
chinmay jain <43497155+chinmay874@users.noreply.github.com>
2020-07-24 23:26:32 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello")
+
+
+# In[6]:
+
+
+print("Hello pyhton")
+
+
+# In[7]:
+
+
+"""   (")... .(")
+( ' o ')
+(")-(")
+(""")-(""") """
+
+
+# In[8]:
+
+
+print("Hello World")
+
+
+# In[9]:
+
+
+print('Python's world')
+
+
+# In[10]:
+
+
+print("Python's world")
+
+
+# In[11]:
+
+
+print('Python\'s world')
+
+
+# In[12]:
+
+
+print("Hello\tPython")
+
+
+# In[13]:
+
+
+print("Hello\nPython")
+
+
+# In[14]:
+
+
+print('Python\'s world')
+
+
+# In[15]:
+
+
+print("Hello\aPython")
+
+
+# In[2]:
+
+
+name= "Chinmay"
+marks=90.050
+age=20
+print("The name of person is",name, "marks is",marks, "age is",age)
+
+
+# In[3]:
+
+
+print ("The name of person is %10s marks is %10.2f age is %10d"%(name,marks, age))
+
+
+# In[4]:
+
+
+print(f"The name of person is {name} the marks is {marks} the age is {age}")
+
+
+# In[5]:
+
+
+8**6
+
+
+# In[6]:
+
+
+10+20
+
+
+# In[7]:
+
+
+-10+20
+
+
+# In[8]:
+
+
+10/2
+
+
+# In[9]:
+
+
+10/3
+
+
+# In[10]:
+
+
+10%3
+
+
+# In[11]:
+
+
+10//3
+
+
+# In[12]:
+
+
+a=10
+bb=30
+c=bb
+
+
+# In[13]:
+
+
+a==c
+
+
+# In[14]:
+
+
+c<a
+
+
+# In[15]:
+
+
+a<c
+
+
+# In[16]:
+
+
+c>a
+
+
+# In[17]:
+
+
+c>=a
+
+
+# In[18]:
+
+
+a!=b
+
+
+# In[19]:
+
+
+a!=c
+
+
+# In[20]:
+
+
+a = get_ipython().getoutput('c')
+
+
+# In[23]:
+
+
+str1= "pythonprogramming"
+"ar" in str1
+
+
+# In[24]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[26]:
+
+
+a is b
+
+
+# In[27]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[28]:
+
+
+x is y
+
+
+# In[29]:
+
+
+id(x)
+
+
+# In[30]:
+
+
+id(y)
+
+
+# In[31]:
+
+
+a=257
+b=257
+a is b
+
+
+# In[32]:
+
+
+a==b
+
+
+# In[33]:
+
+
+not a==b
+
+
+# In[34]:
+
+
+10>8>9
+
+
+# In[ ]:
+
+
+
+
